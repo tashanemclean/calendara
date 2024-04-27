7e902bd4dd5228ec8628cb9f3e9bde439dbfb04f
Tashane Mclean <tashane.mclean@gmail.com>
2024-04-27 13:46:38 -0400

    chore(Housekeeping): cleanup dayjs extends

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,11 +1,16 @@
-import dayjs, { Dayjs } from 'dayjs';
+import type { Dayjs } from 'dayjs';
 import { ReactNode, useEffect, useState } from 'react';
 import locale from 'dayjs/locale/en';
+import dayjs from 'dayjs';
 import weekdayPlugin from 'dayjs/plugin/weekday';
 import objectPlugin from 'dayjs/plugin/toObject';
 import isTodayPlugin from 'dayjs/plugin/isToday';
-import styled from 'styled-components';
+
 import { NextArrowIcon, PrevArrowIcon } from '../Icons';
+
+dayjs.extend(weekdayPlugin);
+dayjs.extend(objectPlugin);
+dayjs.extend(isTodayPlugin);
 
 interface WeekDates {
   dates: {
@@ -20,9 +25,7 @@ export const Calendar = () => {
   const now = dayjs().locale({
     ...locale,
   });
-  dayjs.extend(weekdayPlugin);
-  dayjs.extend(objectPlugin);
-  dayjs.extend(isTodayPlugin);
+
   const [currentMonth, setCurrentMonth] = useState(now);
   const [arrayOfDays, setArrayOfDays] = useState<WeekDates[]>([]);
 
@@ -159,124 +162,3 @@ export const Calendar = () => {
     </div>
   );
 };
-
-// .calendar
-const Container = styled.div`
-  display: block;
-  position: relative;
-  width: 100%;
-  background: var(--neutral-color);
-  border: 1px solid var(--border-color);
-`;
-
-// .header
-const Header = styled.div`
-  text-transform: uppercase;
-  font-weight: 700;
-  font-size: 115%;
-  padding: 1.5em 0;
-  border-bottom: 1px solid var(--border-color);
-`;
-
-const Icon = styled.div`
-  cursor: pointer;
-  transition: 0.15s ease-out;
-
-  &:hover {
-    transform: scale(1.75);
-    transition: 0.25s ease-out;
-    color: var(--main-color);
-  }
-  &:first-of-type {
-    margin-left: 1em;
-  }
-  &:last-of-type {
-    margin-right: 1em;
-  }
-`;
-
-// .days
-const Days = styled.div`
-  text-transform: uppercase;
-  font-weight: 400;
-  color: var(--text-color-light);
-  font-size: 70%;
-  padding: 0.75em 0;
-  border-bottom: 1px solid var(--border-color);
-`;
-
-// .body
-const Body = styled.div`
-  font-family: 'Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
-  font-size: 1em;
-  font-weight: 300;
-  line-height: 1.5;
-  color: var(--text-color);
-  background: var(--bg-color);
-  position: relative;
-`;
-
-const BodyCell = styled.div`
-  position: relative;
-  height: 5em;
-  border-right: 1px solid var(--border-color);
-  overflow: hidden;
-  cursor: pointer;
-  background: var(--neutral-color);
-  transition: 0.25s ease-out;
-
-  &:hover {
-    background: var(--bg-color);
-    transition: 0.5s ease-out;
-  }
-  &:last-child {
-    border-right: none;
-  }
-`;
-
-const BodyCellNumber = styled.span`
-  position: absolute;
-  font-size: 82.5%;
-  line-height: 1;
-  top: 0.75em;
-  right: 0.75em;
-  font-weight: 700;
-`;
-
-const BodyCellBackground = styled.div`
-  font-weight: 700;
-  line-height: 1;
-  color: var(--main-color);
-  opacity: 0;
-  font-size: 8em;
-  position: absolute;
-  top: -0.2em;
-  right: -0.05em;
-  transition: 0.25s ease-out;
-  letter-spacing: -0.07em;
-`;
-
-const BodyColumn = styled.div`
-  flex-grow: 0;
-  flex-basis: calc(100% / 7);
-  width: calc(100% / 7);
-`;
-
-const BodyDisabled = styled.div`
-  color: var(--text-color-light);
-  pointer-events: none;
-`;
-
-const BodySelected = styled.div`
-  border-left: 10px solid transparent;
-  border-image: linear-gradient(45deg, #e7cc65 0%, #f7d655 40%);
-  border-image-slice: 1;
-`;
-
-const BodyRow = styled.div`
-  border-bottom: 1px solid var(--border-color);
-
-  &:last-child {
-    border-bottom: none;
-  }
-`;
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,20 +3,13 @@ import customParseFormat from 'dayjs/plugin/customParseFormat';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
 import localeData from 'dayjs/plugin/localeData';
-import weekdayPlugin from 'dayjs/plugin/weekday';
-import objectPlugin from 'dayjs/plugin/toObject';
-import isTodayPlugin from 'dayjs/plugin/isToday';
 
 // configure Dayjs with extensions
-// dayjs.extend(utc);
-// dayjs.extend(customParseFormat);
-// dayjs.extend(timezone);
-// dayjs.extend(localeData);
-// dayjs().localeData();
-
-dayjs.extend(weekdayPlugin);
-dayjs.extend(objectPlugin);
-dayjs.extend(isTodayPlugin);
+dayjs.extend(utc);
+dayjs.extend(customParseFormat);
+dayjs.extend(timezone);
+dayjs.extend(localeData);
+dayjs().localeData();
 
 type DateFormat = string;
 
