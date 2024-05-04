40c74908c6ee26424af2f71325868d955155d7bf
Tashane Mclean <tashane.mclean@gmail.com>
2024-05-04 15:16:33 -0400

    omit allow origins

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,6 @@ const api = axios.create({ withCredentials: true });
 
 api.defaults.headers.common['Content-Type'] = 'application/json';
 api.defaults.headers.common.Accept = 'application/json';
-api.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 api.defaults.baseURL = import.meta.env.VITE_API_BASE_URL as string;
 
 api.interceptors.request.use((config) => {
